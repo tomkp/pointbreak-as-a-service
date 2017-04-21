7ca91891358d14780861d2c348d7f88828a8ae7c
tomkp <tom@tomkp.com>
2017-04-21 15:37:40 +0100

    serve line

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-module.exports = () => 'Point Break as a service';
+
+const pointbreak = require('./pointbreak.js');
+
+console.log(``, pointbreak.length);
+
+
+
+module.exports = (req, res) => {
+  console.log(``, req.headers.host);
+  if (req.url && req.url.length > 1) {
+    const index = req.url.substring(1);
+    if (index > pointbreak.length - 1) {
+      res.end('The End');
+    } else {
+      res.end(pointbreak[index]);
+    }
+  } else {
+    res.end(`Point Break as a service. <a href="${req.headers.host}/0">${req.headers.host}/0</a>`);
+  }
+
+};
