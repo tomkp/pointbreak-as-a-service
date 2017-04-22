82bbe1be4b6f42d2806181f9d970edde90f37fae
tomkp <tom@tomkp.com>
2017-04-22 20:11:14 +0100

    ...

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const pointbreak = require('./pointbreak.js');
-const {send} = require('micro')
+const { send } = require('micro');
 
 module.exports = async (req, res) => {
   if (req.url && req.url.length > 1) {
     const index = req.url.substring(1);
     const max = pointbreak.length - 1;
     if (index > max) {
-      send(res, 400, `Max value is ${max}`)
+      send(res, 400, `Max value is ${max}`);
     } else {
       res.end(pointbreak[index]);
     }
   } else {
-    res.end(`<html>Point Break as a service. <p><a href="https://${req.headers.host}/0">https://${req.headers.host}/0</a></html>`);
+    res.end(
+      `<html>Point Break as a service. <p><a href="https://${req.headers.host}/0">https://${req.headers.host}/0</a></html>`
+    );
   }
 };
